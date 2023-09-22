6716200413340d920216ae355e792f5879c216f9
iz-Brum <izann.brum@alunosenai.mt>
2023-09-22 03:43:44 -0400

    Add comentários oa index do desafio 009

--- desafios/d009/index.php
+++ desafios/d009/index.php
@@ -1,3 +1,11 @@
+<!-- 
+
+Este código HTML com PHP cria uma página que permite ao usuário inserir dois valores e seus respectivos pesos. 
+Ao submeter o formulário, a página calcula a média aritmética simples e a média aritmética ponderada dos valores. 
+Os valores inseridos pelo usuário e seus pesos são pré-definidos na segunda vez que o formulário é exibido (retroalimentado). O resultado é exibido na seção "Cálculo das Médias".
+
+-->
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,10 +16,11 @@
 </head>
 <body>
     <?php 
+        // Capturando os dados do formulário retroalimentado
         $primeiroValor = $_GET['primeiroValor'] ?? 0; 
         $segundoValor = $_GET['segundoValor'] ?? 0;
         $pesoPrimeiroValor = $_GET['pesoPrimeiroValor'] ?? 0;
-        $pesoSegundoValor = $_GET['pesoSegundoValor']?? 0;
+        $pesoSegundoValor = $_GET['pesoSegundoValor'] ?? 0;
     ?>
 
     <main style="margin-top: 1px">
@@ -23,16 +32,16 @@
             <!-- Campo de entrada para o primeiro valor, com valor pré-definido -->
             <input type="number" name="primeiroValor" id="id-primeiroValor" value="<?=$primeiroValor?>">
 
-            <label for="pesoPrimeiroValor">1° Valor:</label>
-            <!-- Campo de entrada para o primeiro valor, com valor pré-definido -->
+            <label for="pesoPrimeiroValor">Peso 1° Valor:</label>
+            <!-- Campo de entrada para o peso do primeiro valor, com valor pré-definido -->
             <input type="number" name="pesoPrimeiroValor" id="id-pesoPrimeiroValor" value="<?=$pesoPrimeiroValor?>">
         
             <label for="segundoValor">2° Valor:</label>
-            <!-- Campo de entrada para o segundoValor, com valor pré-definido -->
+            <!-- Campo de entrada para o segundo valor, com valor pré-definido -->
             <input type="number" name="segundoValor" id="id-segundoValor" value="<?=$segundoValor?>">
 
-            <label for="pesoSegundoValor">1° Valor:</label>
-            <!-- Campo de entrada para o primeiro valor, com valor pré-definido -->
+            <label for="pesoSegundoValor">Peso 2° Valor:</label>
+            <!-- Campo de entrada para o peso do segundo valor, com valor pré-definido -->
             <input type="number" name="pesoSegundoValor" id="id-pesoSegundoValor" value="<?=$pesoSegundoValor?>">
             
             <!-- Botão para submeter o formulário -->
@@ -44,19 +53,14 @@
         <h2>Cálculo das Médias</h2>
         
         <?php 
-            
+            // Calcula a média aritmética simples e a média aritmética ponderada
             $mediaSimples = ($primeiroValor + $segundoValor) / 2;
-            $mediaPonderada = (($primeiroValor*$pesoPrimeiroValor) + ($segundoValor*$pesoSegundoValor))/($pesoPrimeiroValor + $pesoSegundoValor);
+            $mediaPonderada = (($primeiroValor * $pesoPrimeiroValor) + ($segundoValor * $pesoSegundoValor)) / ($pesoPrimeiroValor + $pesoSegundoValor);
             
-            // Exibe o resultado da análise da divisão
-            echo "<p>Analisando os valores $primeiroValor e $segundoValor: <ul><li> A <strong>Média Aritmética Simples</strong> entre os valores é igual a <strong>". number_format($mediaSimples, 2, ",", ".") ."</strong><li> A <strong>Média Aritmética Ponderada</strong> pesos $pesoPrimeiroValor e $pesoSegundoValor é igual a <strong>". number_format($mediaPonderada, 2, ",", ".") ."</strong></ul></p>";
+            // Exibe o resultado das médias
+            echo "<p>Analisando os valores $primeiroValor e $segundoValor: <ul><li> A <strong>Média Aritmética Simples</strong> entre os valores é igual a <strong>". number_format($mediaSimples, 2, ",", ".") ."</strong></li><li> A <strong>Média Aritmética Ponderada</strong> pesos $pesoPrimeiroValor e $pesoSegundoValor é igual a <strong>". number_format($mediaPonderada, 2, ",", ".") ."</strong></li></ul></p>";
         ?>
     </section>
 </body>
 </html>
-
-<!-- 
-
-Este código HTML com PHP cria uma página que permite ao usuário inserir um primeiroValor e um segundoValor. Ao submeter o formulário, a página exibe o quociente e o resto da divisão. Os valores inseridos pelo usuário são pré-definidos na segunda vez que o formulário é exibido (retroalimentado). O resultado da análise da divisão é exibido na seção "Estrutura da Divisão".
-
- -->
+<!-- Fim do código -->
