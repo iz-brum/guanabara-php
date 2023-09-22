4e73af7a65deee7abdfd1492bde560081c9e164a
iz-Brum <izann.brum@alunosenai.mt>
2023-09-22 02:54:37 -0400

    Add comentários ao index do desafio 006

--- desafios/d006/index.php
+++ desafios/d006/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <?php 
-        // Capturando os dados do formulário retroalimentado
+        // Capturando os valores de dividendo e divisor do formulário retroalimentado
         $dividendo = $_GET['dividendo'] ?? 0; 
         $divisor = $_GET['divisor'] ?? 1;
     ?>
@@ -20,12 +20,12 @@
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="dividendo">Dividendo:</label>
             
-            <!-- Campo de entrada para o primeiro valor, com valor pré-definido -->
+            <!-- Campo de entrada para o dividendo, com valor pré-definido -->
             <input type="number" name="dividendo" id="id-dividendo" value="<?=$dividendo?>">
             
             <label for="divisor">Divisor:</label>
             
-            <!-- Campo de entrada para o segundo valor, com valor pré-definido -->
+            <!-- Campo de entrada para o divisor, com valor pré-definido -->
             <input type="number" name="divisor" id="id-divisor" value="<?=$divisor?>">
             
             <!-- Botão para submeter o formulário -->
@@ -37,12 +37,11 @@
         <h2>Estrutura da Divisão</h2>
         
         <?php 
-            
-            $quociente = (int) ($dividendo/$divisor);
-            // $quocienteInteiro =  $quociente;
+            // Calcula o quociente e o resto da divisão
+            $quociente = (int) ($dividendo / $divisor);
             $resto = $dividendo % $divisor;
             
-            // Exibe o resultado da soma
+            // Exibe o resultado da análise da divisão
             echo "<p> $dividendo : $divisor --> q: <strong> $quociente</strong> e r: <strong>$resto</strong>.</p>";
         ?>
     </section>
@@ -51,6 +50,6 @@
 
 <!-- 
 
-Este código HTML com PHP cria um formulário para inserir dois valores, calcular a soma e exibir o resultado. Ele usa o método GET para enviar os dados e a mesma página PHP para processar os valores. Os valores inseridos pelo usuário são pré-definidos na segunda vez que o formulário é exibido (retroalimentado). O resultado da soma é exibido na seção "Resultado da soma".
+Este código HTML com PHP cria uma página que permite ao usuário inserir um dividendo e um divisor. Ao submeter o formulário, a página exibe o quociente e o resto da divisão. Os valores inseridos pelo usuário são pré-definidos na segunda vez que o formulário é exibido (retroalimentado). O resultado da análise da divisão é exibido na seção "Estrutura da Divisão".
 
  -->
